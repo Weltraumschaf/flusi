186db2625788163388650086bbf305a75d5ceb96
Sven Strittmatter <ich@weltraumschaf.de>
2026-07-15 22:20:22 +0200

    Add Flusi assemblies and passing test harness

---
+++ Assets/Tests/EditMode/HarnessTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Flusi.Tests
+{
+    public class HarnessTest
+    {
+        [Test]
+        public void Harness_Runs()
+        {
+            Assert.Pass();
+        }
+    }
+}
